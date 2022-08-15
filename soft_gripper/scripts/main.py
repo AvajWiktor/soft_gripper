157276972d825948d73baf853a01a6f76ea210b5
Wiktor <wiktor.markowski98@gmail.com>
2022-08-15 21:55:33 +0200

    removed view model and added serial port handler in main model

--- soft_gripper/scripts/main.py
+++ soft_gripper/scripts/main.py
@@ -1,9 +1,12 @@
 import sys
 
+from model.main_model import *
 from view import main_view
 
 if __name__ == '__main__':
 
-    App = main_view.MainWindowView()
-    App.root.destroy()
+    gripper = MainModel()
+
+    #App = main_view.MainWindowView()
+    #App.root.destroy()
     sys.exit("EXIT SUCCESS")
--- soft_gripper/scripts/model/main_model.py
+++ soft_gripper/scripts/model/main_model.py
@@ -7,21 +7,23 @@ import math
 from threading import Thread, currentThread
 import numpy as np
 import skfuzzy as fuzz
+import serial
 
 
 class MainModel(pyCandle.Candle):
     # working PID values 14.0 / 0.01 / 0.3
     # working PID for position 10.0 / 100/ 0.1
-    def __init__(self, root):
+    def __init__(self):
         super().__init__(pyCandle.CAN_BAUD_1M, True)
-        self.root = root
         self.motor_id = 101
-        self.kp = 3.0
+        self.kp = 1.0
         self.temp_kp = self.kp
-        self.ki = 0.01
-        self.kd = 0.01
+        self.ki = 100
+        self.kd = 0.001
         self.windup = 0.0
-        self.output_multiplier = 1.0
+        self.main_thread = Thread(name="MainLoopThread", target=self.main_loop)
+        self.output_multiplier = 0.5
+        self.open_position = -12.48
         self.control_mode = pyCandle.POSITION_PID
         # self.control_mode = pyCandle.IMPEDANCE
         self.desired_torque = 0.0
@@ -30,9 +32,33 @@ class MainModel(pyCandle.Candle):
         self.program_status = self.startup_procedure()
         if not self.program_status:
             self.end()
+        self.main_thread.start()
+        self.main_thread.join()
+
+    def main_loop(self):
+        ser = serial.Serial(
+            port='/dev/ttyUSB0',
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
+        while self.handle_serial_input(ser.readline().decode()):
+            try:
+                time.sleep(1)
+            except KeyboardInterrupt:
+                break
 
     def set_encoders_to_zero(self):
         return self.controlMd80SetEncoderZero(self.md80s[0])
+
+    def handle_serial_input(self, input):
+        print(f"input: {input}")
+        if input == 'x':
+            return False
+
+        return True
 
     def set_grip_flag(self):
         if self.grip_flag:
@@ -53,7 +79,7 @@ class MainModel(pyCandle.Candle):
                 self.md80s[0].setMaxTorque(0.45)
                 self.set_max_velocity(12.48)
 
-                self.set_encoders_to_zero()
+                # self.set_encoders_to_zero()
 
             self.begin()
             return True
@@ -72,11 +98,13 @@ class MainModel(pyCandle.Candle):
             time.sleep(0.001)
             # if (self.desired_torque + 0.03) >= self.get_torque() >= self.desired_torque - 0.03:
             if self.grip_flag:
+                self.set_target_position(self.get_position())
+                self.grip_flag = False
                 break
 
     def open_gripper(self):
         self.grip_flag = True
-        self.md80s[0].setTargetPosition(-12.48)
+        self.md80s[0].setTargetPosition(self.open_position)
         time.sleep(0.1)
         self.grip_flag = False
 
@@ -108,7 +136,7 @@ class MainModel(pyCandle.Candle):
 
     def prepare_fuzzy_rules(self):
         self.torque_error = np.arange(0.0, 0.6, 0.02, dtype=float)
-        position = np.arange(0, 3.0, 0.1, dtype=float)
+        self.position = np.arange(0, 3.0, 0.1, dtype=float)
 
         self.te_zero = fuzz.trapmf(self.torque_error, [0.0, 0.0, 0.0, 0.05])
         self.te_small = fuzz.trapmf(self.torque_error, [0.05, 0.1, 0.1, 0.15])
@@ -117,17 +145,17 @@ class MainModel(pyCandle.Candle):
         self.te_very_large = fuzz.trapmf(self.torque_error, [0.35, 0.40, 0.45, 0.5])
         self.te_edge = fuzz.trapmf(self.torque_error, [0.45, 0.5, 0.5, 0.55])
 
-        self.p_edge = fuzz.trapmf(position, [2.0, 2.5, 2.5, 3.0])
-        self.p_very_large = fuzz.trapmf(position, [1.6, 1.8, 1.8, 2.0])
-        self.p_large = fuzz.trapmf(position, [1.2, 1.4, 1.4, 1.6])
-        self.p_medium = fuzz.trapmf(position, [0.8, 1.0, 1.0, 1.2])
-        self.p_small = fuzz.trapmf(position, [0.4, 0.6, 0.6, 0.8])
-        self.p_zero = fuzz.trapmf(position, [0.0, 0.0, 0.2, 0.4])
+        self.p_edge = fuzz.trapmf(self.position, [2.0, 2.5, 2.5, 3.0])
+        self.p_very_large = fuzz.trapmf(self.position, [1.6, 1.8, 1.8, 2.0])
+        self.p_large = fuzz.trapmf(self.position, [1.2, 1.4, 1.4, 1.6])
+        self.p_medium = fuzz.trapmf(self.position, [0.8, 1.0, 1.0, 1.2])
+        self.p_small = fuzz.trapmf(self.position, [0.4, 0.6, 0.6, 0.8])
+        self.p_zero = fuzz.trapmf(self.position, [0.0, 0.0, 0.2, 0.4])
 
     def calculate_position(self, error_input):
 
         error = abs(error_input)
-        #print(f"Error: {error}")
+        # print(f"Error: {error}")
         sign = -1 if error_input < 0 else 1
 
         R1 = fuzz.interp_membership(self.torque_error, self.te_edge, error)
@@ -153,11 +181,10 @@ class MainModel(pyCandle.Candle):
         # print(predicted_position)
 
         # print(R_combined)
-        position_out = fuzz.defuzz(position, R_combined, 'centroid')
+        position_out = fuzz.defuzz(self.position, R_combined, 'centroid')
 
-        #print(sign * position_out / 10.0)
+        # print(sign * position_out / 10.0)
         return sign * position_out * self.output_multiplier
-
 
     def position_value(self, value):
         """Legacy, do not use"""
